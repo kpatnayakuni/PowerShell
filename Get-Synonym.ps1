f17f44faa39c0358f7c9ed481fb58fbe146dae06
kpatnayakuni <31788728+kpatnayakuni@users.noreply.github.com>
2017-10-28 22:31:58 +0530

    Get Synonym using powershell

--- Get-Synonym.ps1
+++ Get-Synonym.ps1
@@ -3,73 +3,35 @@ Function Get-Synonym
     Param
     (
         [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
-        [string]$Word
+        [String] $Word
     )
-    $Web = New-Object System.Net.WebClient
-    $Web.UseDefaultCredentials = $true
-    $Web.Proxy.Credentials = $Web.Credentials
-    try 
-    {
-        $Data = $Web.DownloadString("http://www.dictionary.com/browse/$Word")
-        $Data = $Data.ToString().Split("`n")
+    
+    $Uri            = "http://www.thesaurus.com/browse/$Word"
+    $ElementTagName = 'span'
+    $ClassName      = 'text'
+
+    try {
+        $Web        = Invoke-WebRequest -Uri $Uri
     }
-    catch 
-    {
-        Write-Host -ForegroundColor Red "Error: No word found! `nCheck the spelling and try again."    
+    catch {
+        Write-Host -ForegroundColor Red "Error:No word found! `nCheckthe spelling and try again."
+        break
     }
-    $Found = $false
-    $OutLines = @()
-    foreach($Line in $Data)
-    {
-        if ($Found)
-        {
-            $Found = $false
-            $OutLine = $Line.Trim()
-            While ($true)
-            {
-                $StartIndex = $OutLine.IndexOf("<")
-                if ($StartIndex -ge 0)
-                {
-                    $EndIndex = $OutLine.IndexOf(">")
-                    if ($EndIndex -lt 0) { Break }
-                    $ReplaceLine = $OutLine.Substring($StartIndex,($EndIndex - $StartIndex) + 1)
-                    $OutLine = $OutLine.Replace($ReplaceLine,'')
-                }
-                else 
-                {
-                    Break    
-                }
-                $OutLines += '>> ' + $OutLine.Trim()
-            }
-        }
-        if($Line.Trim() -eq '<div class="def-content">')
-        {
-            $Found = $true
-        }
-    }
-    $OriginalColor = $Host.UI.RawUI.ForegroundColor
-    $Host.UI.RawUI.ForegroundColor = 10
-    $OutLines | Select -First 3
-    $OutLines = $OutLines | select -Skip 3
-    foreach($PrintLine in $OutLines)
-    {
-        $OrigPos = $Host.UI.RawUI.CursorPosition
-        $Host.UI.RawUI.ForegroundColor = $OriginalColor
-        Write-Host -NoNewline " -- More -- (Ctrl+C to exit)"
-        $KeyEntered = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp")
-        if ($KeyEntered.VirtualKeyCode -eq 17)
-        {
-            $KeyHold = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp")
-            if ($KeyHold.VirtualKeyCode -eq 67)
-            {
-                Break
-            }
-        }
-        $Host.UI.RawUI.CursorPosition = $OrigPos
-        ' ' * 30
-        $Host.UI.RawUI.CursorPosition = $OrigPos
-        $Host.UI.RawUI.ForegroundColor = 2
-        $PrintLine
-    }
+
+    $Elements       = $Web.ParsedHtml.getElementsByTagName($ElementTagName)
+    $Synonyms       = $Elements | Where-Object { ($_.className -eq $ClassName) -and ($_.nextSibling -ne $null) }
+    $Antonyms       = $Elements | Where-Object { ($_.className -eq $ClassName) -and ($_.nextSibling -eq $null) }
+
+    $OriginalColor  = $Host.UI.RawUI.ForegroundColor
+    Write-Host -ForegroundColor Yellow 'Synonyms for ' -NoNewline
+    Write-Host $Word
+    $Host.UI.RawUI.ForegroundColor = 'Green'
+    $Synonyms | Select-Object innerText | Format-Wide -Column 6
+    $Host.UI.RawUI.ForegroundColor = $OriginalColor
+
+    Write-Host -ForegroundColor Yellow 'Antonyms for ' -NoNewline
+    Write-Host $Word
+    $Host.UI.RawUI.ForegroundColor = 'Red'
+    $Antonyms | Select-Object innerText | Format-Wide -Column 6
     $Host.UI.RawUI.ForegroundColor = $OriginalColor
 }
