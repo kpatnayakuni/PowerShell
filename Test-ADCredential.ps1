28bd30c88ab97054b355f9e8e7d0311135554009
kpatnayakuni <31788728+kpatnayakuni@users.noreply.github.com>
2017-11-06 21:04:56 +0530

    Validates domain credentials

---
+++ Test-ADCredential.ps1
@@ -0,0 +1,17 @@
+﻿function Test-ADCredential 
+{
+    $Credentials = Get-Credential -Message 'Enter your domain credentials'
+    $UserName = $Credentials.UserName
+    $Password = $Credentials.GetNetworkCredential().Password
+    Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+    $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('domain')
+    try 
+    {
+        $Status = $DS.ValidateCredentials($UserName, $Password)
+        if ($Status) { Write-Host -ForegroundColor Green "Credentials authenticated successfully!" }
+        else { Write-Host -ForegroundColor Red "Credentials authentication failed!" }
+    }
+    catch { Write-Host -ForegroundColor Red "Credentials authentication failed!" }
+}
+
+Test-ADCredential 
