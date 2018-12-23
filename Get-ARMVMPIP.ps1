f0c40a1f0672c5ab15c6e86848bf8a73e1054048
Kiran P <kpatnayakuni@Inspire2.patnayakuni.in>
2018-12-23 15:40:01 +0530

    Get-ARMVMPIP.ps1

--- Get-ARMVMPIP.ps1
+++ Get-ARMVMPIP.ps1
@@ -11,6 +11,9 @@ param
     [Parameter(Mandatory=$false,ParameterSetName='Name')]
     [Parameter(Mandatory=$false,ParameterSetName='Object')]
     [switch] $StartIfVMIsNotRunning,
+    [Parameter(Mandatory=$false,ParameterSetName='Name')]
+    [Parameter(Mandatory=$false,ParameterSetName='Object')]
+    [switch] $ConnetRDP,
     [Parameter(Mandatory=$true,ParameterSetName='Help')]
     [switch] $H
 )
@@ -32,36 +35,27 @@ if ($ModuleVersion -lt $RequiredModuleVersion)
 if([string]::IsNullOrEmpty($(Get-AzureRmContext)))
 { $null = Add-AzureRmAccount }
 
-do 
+try
 {
-    try
-    {
-        if ($PSCmdlet.ParameterSetName -eq 'Name')
-        { [Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine] $VM = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VMName -Status}
-        elseif ($PSCmdlet.ParameterSetName -eq 'Object') { [Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine] $VM = Get-AzureRmVM -ResourceGroupName $VMObject.ResourceGroupName -Name $VMObject.Name -Status }
-    }
-    catch
-    {
-        Write-Verbose -Message $_.Exception.Message
-        break
-    }
+    if ($PSCmdlet.ParameterSetName -eq 'Name')
+    { [Microsoft.Azure.Commands.Compute.Models.PSVirtualMachineInstanceView] $VM = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VMName -Status}
+    elseif ($PSCmdlet.ParameterSetName -eq 'Object') { [Microsoft.Azure.Commands.Compute.Models.PSVirtualMachineInstanceView] $VM = Get-AzureRmVM -ResourceGroupName $VMObject.ResourceGroupName -Name $VMObject.Name -Status }
+}
+catch
+{
+    Write-Verbose -Message $_.Exception.Message
+    break
+}
 
-    [Microsoft.Azure.Management.Compute.Models.InstanceViewStatus] $VMStatus = $VM.Statuses | Where-Object { $_.Code -match 'running' }
+[Microsoft.Azure.Management.Compute.Models.InstanceViewStatus] $VMStatus = $VM.Statuses | Where-Object { $_.Code -match 'running' }
 
-    if ([string]::IsNullOrEmpty($VMStatus))
-    {
-        Write-Verbose -Message "Since {0} VM is not running, cannot determine the public IP unless 'Public IP Allocation Method' is static" -f $VMName
-        [bool] $ISVMRunning = $false
-    }
-    else { [bool] $ISVMRunning = $true }
+if ([string]::IsNullOrEmpty($VMStatus)) { [bool] $ISVMRunning = $false } else { [bool] $ISVMRunning = $true }
 
-    if ($ISVMRunning -eq $false -and $StartIfVMIsNotRunning -eq $true)
-    {
-        Start-AzureRMVM -ResourceGroupName 
-    }
-} while ($true) 
+if ($ISVMRunning -eq $false -and $StartIfVMIsNotRunning -eq $true)
+{ $null = Start-AzureRMVM -ResourceGroupName $VM.ResourceGroupName -Name $VM.Name } 
 
-[string] $NICId = $VM.NetworkProfile.NetworkInterfaces.id
+[Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine] $VirtualMachine = Get-AzureRMVM -ResourceGroupName $VM.ResourceGroupName -Name $VM.Name
+[string] $NICId = $VirtualMachine.NetworkProfile.NetworkInterfaces.id
 [Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResource] $NICResource = Get-AzureRmResource -ResourceId $NICId
 [string] $PIPId = $NICResource.Properties.ipConfigurations.properties.publicIPAddress.id
 [Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels.PSResource] $PIPResource = Get-AzureRmResource -ResourceId $PIPId
@@ -71,6 +65,14 @@ do
 
 if ([string]::IsNullOrEmpty($PIP.IPAddressToString) -and $ISVmRunning -eq $false -and $PublicIPAllocationMethod -eq 'Dynamic')
 {
-    Write-Verbose -Message "Since {0} VM is not running, unable to determine the Public IP"
+    Write-Verbose -Message $("Since {0} VM is not running and 'Public IP Allocation Method is Dynamic', unable to determine the Public IP.`nRun the command with -StartIfVMIsNotRunning flag" -f $VMName)
+    break
 }
+
+if ($ConnetRDP)
+{
+    Invoke-Expression "mstsc.exe /v $($PIP.IPAddressToString)"
+    break
+}
+
 return, $PIP.IPAddressToString
