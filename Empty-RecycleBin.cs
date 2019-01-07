a668403a5f2d799376d888843875d646ef4819bb
Kiran P <kpatnayakuni@Inspire2.patnayakuni.in>
2019-01-07 08:34:47 +0530

    NS Change

--- Empty-RecycleBin.cs
+++ Empty-RecycleBin.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Runtime.InteropServices;
 
-namespace MyPC
+namespace MyComputer
 {
-    public static class RB
+    public static class RecycleBin
     {
         enum RecycleFlags : int
         {
