c874dbd030c415fe572a3b5bfb7c699c69759e45
Patnayakuni <kiran.patnayakuni@accenture.com>
2019-01-14 00:04:01 +0530

    Demo tips

--- Demo-CalculatedFields.ps1
+++ Demo-CalculatedFields.ps1
@@ -1,3 +1,28 @@
-Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -Property Caption, TotalVisibleMemorySize, LastBootUpTime
+# Get the total memory in GB from the local computer using the calculated property with Select-Object
+Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -Property PSComputerName, `
+@{Name = 'Memory in GB'; Expression = {[Math]::Round($_.TotalVisibleMemorySize/1MB)}}
 
-Get-Service -Name App* | Select-Object 
+# Output
+PSComputerName Memory in GB
+-------------- ------------
+Workstation               8
+
+# Get the services where the names are starting with App, and display IsRunning with Yes/No using the calculated property
+$IsRunning = @{
+    Label = "IsRunning"
+    Expression = {
+        if($_.Status -eq 'Running') { "Yes" }
+        else { "No" }
+    }
+}
+Get-Service -Name App* | Select-Object -Property Name, DisplayName, $IsRunning
+
+#Output
+Name         DisplayName                       IsRunning
+----         -----------                       ---------
+AppIDSvc     Application Identity              No
+Appinfo      Application Information           Yes
+AppMgmt      Application Management            No
+AppReadiness App Readiness                     No
+AppVClient   Microsoft App-V Client            No
+AppXSvc      AppX Deployment Service (AppXSVC) No
