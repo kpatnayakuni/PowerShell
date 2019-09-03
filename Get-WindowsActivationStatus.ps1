9fdf04bbc217893e9c9f9c87509cd371d16e7145
kpatnayakuni <31788728+kpatnayakuni@users.noreply.github.com>
2019-09-03 16:45:28 +0530

    Comments

--- Get-WindowsActivationStatus.ps1
+++ Get-WindowsActivationStatus.ps1
@@ -1,7 +1,7 @@
 Function Get-WinSrvFromInv
 {
     <#
-        The purpose of this function is to retrieve the list of server for which you want to check the Windows Activation status.
+        The purpose of this function is to retrieve the list of servers for which you want to check the Windows Activation status.
         Write your piece of code to retirve the servers from your inventory.
         for example, Get-Content -Path $InvPath\Server.txt
     #>
@@ -9,6 +9,12 @@ Function Get-WinSrvFromInv
 }
 Function Get-WindowsActivation 
 {
+    <#
+        This is a PowerShell advanced function to rerieve the Windows Activation Status using CIM classes.
+        It takes one or more servers as an input, and it accepts through pipeline as well
+        This script will check the connectivity, and then checks the activation status
+        It collects the infor from all the servers and then return the value of error at once.
+    #>
     [CmdLetBinding()]
     Param
     (
@@ -45,4 +51,5 @@ Function Get-WindowsActivation
     }
 }
 
+## Invoke the functions.
 Get-WinSrvFromInv | Get-WindowsActivation
