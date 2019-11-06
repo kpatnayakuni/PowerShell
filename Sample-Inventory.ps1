193dced68f0e8ef3d3e3bfd0b100f6c690e1affc
kpatnayakuni <31788728+kpatnayakuni@users.noreply.github.com>
2019-11-06 10:54:56 +0530

    Sample Inventory

---
+++ Sample-Inventory.ps1
@@ -0,0 +1,39 @@
+
+$Computername = 'localhost'
+
+$OS     = Get-CimInstance -ClassName CIM_OperatingSystem -ComputerName $Computername
+$CS     = Get-CimInstance -ClassName CIM_ComputerSystem -ComputerName $Computername
+$CPUs   = Get-CimInstance -ClassName CIM_Processor -ComputerName $Computername
+$CPUInfo= @()
+foreach ($CPU in $CPUs) 
+{
+    $CPUInfo += [hashtable]@{
+        DeviceID    = $CPU.DeviceID
+        Name        = $CPU.Caption 
+    }
+}
+$BIOS       = Get-CimInstance -ClassName CIM_BIOSElement -ComputerName $Computername
+$Disks      = Get-CimInstance -ClassName CIM_LogicalDisk -ComputerName $Computername
+$DiskInfo   = @()
+foreach ($Disk in $Disks)
+{
+    if ($Disk.Access -ne 0) { continue }
+    $DiskInfo += [hashtable]@{
+        Drive       = $Disk.Name
+        Size        = [math]::Round($Disk.Size/1GB)
+        FreeSpace   = [math]::Round($Disk.FreeSpace/1GB)
+    }
+}
+$User = Invoke-Command -ScriptBlock { "$($ENV:USERDOMAIN)\$($ENV:USERNAME)" } -ComputerName $Computername
+
+$Output = New-Object -TypeName psobject -Property @{
+    ComputerName            = $Computername
+    CurrentlyLoggedOnUser   = $User
+    OperatingSystem         = $OS.Caption
+    CPUInfo                 = $CPUInfo
+    DiskInfo                = $DiskInfo
+    TotalPhysicalMemory     = [math]::Round($CS.TotalPhysicalMemory/1GB)
+    ServiceTag              = $BIOS.SerialNumber
+}
+
+return, $Output
