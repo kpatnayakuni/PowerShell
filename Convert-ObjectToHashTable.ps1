7b525422f98dcf140cd2d1c3388bfd7fc0b1b122
= <=>
2019-11-26 21:55:24 +0530

    Added Comment

--- Convert-ObjectToHashTable.ps1
+++ Convert-ObjectToHashTable.ps1
@@ -7,13 +7,11 @@ Function Convert-ObjectToHashTable
         [parameter(Mandatory=$true,ValueFromPipeline=$true)]
         [pscustomobject] $Object
     )
-
     $HashTable = @{}
     $ObjectMembers = Get-Member -InputObject $Object -MemberType *Property
     foreach ($Member in $ObjectMembers) 
     {
         $HashTable.$($Member.Name) = $Object.$($Member.Name)
     }
-
     return $HashTable
 }
