c340dcab18154f717ffe6374e88000e7ec01a35b
Kiran Patnayakuni <kiran@patnayakuni.com>
2021-05-07 10:29:27 +0530

    HBD Song

---
+++ General/Sing-HappyBirthDay.ps1
@@ -0,0 +1,40 @@
+# https://www.reddit.com/r/PowerShell/comments/n5j3ic/happy_birthday_song_with_beep_tones_in_powershell/
+
+# Happy Birthday song with Beep tones in Powershell Script
+
+$BeepList = @(
+    @{ Pitch = 1059.274; Length = 300; };
+    @{ Pitch = 1059.274; Length = 200; };
+    @{ Pitch = 1188.995; Length = 500; };
+    @{ Pitch = 1059.274; Length = 500; };
+    @{ Pitch = 1413.961; Length = 500; };
+    @{ Pitch = 1334.601; Length = 950; };
+
+    @{ Pitch = 1059.274; Length = 300; };
+    @{ Pitch = 1059.274; Length = 200; };
+    @{ Pitch = 1188.995; Length = 500; };
+    @{ Pitch = 1059.274; Length = 500; };
+    @{ Pitch = 1587.117; Length = 500; };
+    @{ Pitch = 1413.961; Length = 950; };
+
+    @{ Pitch = 1059.274; Length = 300; };
+    @{ Pitch = 1059.274; Length = 200; };
+    @{ Pitch = 2118.547; Length = 500; };
+    @{ Pitch = 1781.479; Length = 500; };
+    @{ Pitch = 1413.961; Length = 500; };
+    @{ Pitch = 1334.601; Length = 500; };
+    @{ Pitch = 1188.995; Length = 500; };
+    @{ Pitch = 1887.411; Length = 300; };
+    @{ Pitch = 1887.411; Length = 200; };
+    @{ Pitch = 1781.479; Length = 500; };
+    @{ Pitch = 1413.961; Length = 500; };
+    @{ Pitch = 1587.117; Length = 500; };
+    @{ Pitch = 1413.961; Length = 900; };
+    );
+
+# I Just added this For..loop in order to listen the beep tones twice (-_°)
+For ($i=1; $i -le 2; $i++) {
+    foreach ($Beep in $BeepList) {
+        [System.Console]::Beep($Beep['Pitch'], $Beep['Length']);
+    }
+}
